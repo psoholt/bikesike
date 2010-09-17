fd0125f82b6d196a30221816668fb8c4e96af232
psoholt <psoeholt@yahoo.no>
2010-09-17 17:01:14 +0200

    Endring

--- app/helpers/execute.rb
+++ app/helpers/execute.rb
@@ -2,7 +2,7 @@
 require 'open-uri'
 require 'rexml/document'
 include REXML
-
+require "../models/bike_station"
 class Execute
   public
   def self.webOpenXML(number)
@@ -43,31 +43,8 @@ class Execute
 
 end
 
-class BikeStation
-  attr_reader :online, :ready_bikes, :empty_locks
 
-  #@online = "hei"
-  #@@static_variable = "statisk variabel"
-  #$global_variable = "global variabel"
-
-  def initialize(doc)
-    doc.elements.each('string/station/online') {|tag| @online = tag.text}
-    doc.elements.each('string/station/ready_bikes') {|tag| @ready_bikes = tag.text}
-    doc.elements.each('string/station/empty_locks') {|tag| @empty_locks = tag.text}
-    doc.elements.each('string/station/description') {|tag| @description = tag.text}
-    doc.elements.each('string/station/longitute') {|tag| @longitude = tag.text}
-    doc.elements.each('string/station/latitude') {|tag| @latitude = tag.text}
-  end
-
-  def to_s
-
-    "Online: "+ "0".eql?(@online).to_s + ",  ReadyBikes: " + @ready_bikes + ", EmptyLocks: " + @empty_locks + \
- ", Posisjon: " + @longitude +", " + @latitude + "\n\t" + @description
-  end
-end
-
-
-#bike = Execute.get_bike_station { Execute.file_open_xml(3) }
-#puts bike
+bike = Execute.get_bike_station { Execute.file_open_xml(3) }
+puts bike
 
 
