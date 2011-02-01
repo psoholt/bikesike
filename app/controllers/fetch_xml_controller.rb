86342e0f560121cab74de886108486334bae616e
Peder Soholt <psoeholt@yahoo.no>
2011-02-01 17:49:50 +0100

    ..

--- app/controllers/fetch_xml_controller.rb
+++ app/controllers/fetch_xml_controller.rb
@@ -2,6 +2,8 @@ require 'open-uri'
 
 class FetchXmlController < ApplicationController
 
+  @bikehash = Hash.new
+
   def fetchxml(id)
     return if id.is_a? int
     bike = Execute.get_bike_station(id) { |id| Execute.web_open_xml(id) }
